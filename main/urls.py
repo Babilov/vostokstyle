76ef3f1a9495e029412a8f374fcab341722976cc
Babilov <egor-kun2017@mail.ru>
2022-10-20 14:33:03 +0300

    add item detail

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import IndexView
+from .views import ItemListView, ItemDetailView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='home')
+    path('', ItemListView.as_view(), name='home'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Burner
+from django.views.generic import ListView, DetailView
+from .models import Burner, Category
 
 
-class IndexView(ListView):
-    queryset = Burner.objects.all()
+class ItemListView(ListView):
+    queryset = Burner.objects.order_by('-upload_at')
     context_object_name = 'data'
     template_name = 'main/index.html'
+
+    def get_queryset(self):
+        queryset = {
+            'burners': Burner.objects.all(),
+            'categories': Category.objects.all()
+        }
+        return queryset
+
+
+class ItemDetailView(DetailView):
+    model = Burner
+    template_name = 'main/item_detail.html'
